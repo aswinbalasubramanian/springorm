c18d9d7894e0946467c760477f383c3f938e14dc
Aswin <aswin.balasubramanian@agnity.com>
2023-12-19 07:18:08 +0530

    HibernateTemplate and configuration of it

--- src/main/java/com/aswin/spring/springorm/product/dao/impl/ProductDaoImpl.java
+++ src/main/java/com/aswin/spring/springorm/product/dao/impl/ProductDaoImpl.java
@@ -1,13 +1,16 @@
 package com.aswin.spring.springorm.product.dao.impl;
 
-import org.springframework.orm.hibernate5.HibernateCallback;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.hibernate5.HibernateTemplate;
+import org.springframework.stereotype.Component;
 
 import com.aswin.spring.springorm.product.dao.Product;
 import com.aswin.spring.springorm.product.dao.ProductDao;
 
+@Component
 public class ProductDaoImpl implements ProductDao {
 
+	@Autowired
 	HibernateTemplate hibernateTemplate;
 
 	@Override
